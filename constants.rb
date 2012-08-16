124d5e2aab758698f57d45e6466bdd9b697b8cbb
John Dzak <john.dzak@gmail.com>
2012-08-16 10:28:10 -0500

    Forgot to add constants file.

---
+++ constants.rb
@@ -0,0 +1,9 @@
+module Constants
+  IOS = "/Users/johndzak/Library/Application\ Support/iPhone\ Simulator/5.0/Applications/BC627ECF-83B3-41AE-87E3-3F3BA5C5E066/Documents/main.sqlite"
+
+  def command(desc, &block)
+    yield
+    r = ($? == 0) ? "Success" : "Failed"
+    puts "#{r}: #{desc}"
+  end
+end
